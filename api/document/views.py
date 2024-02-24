d3ed6ac0865956ae4d862e2a22f133279b2eb655
YusungAN <52990109+YusungAN@users.noreply.github.com>
2024-02-25 03:23:34 +0900

    문단 단위로 추출

--- api/document/views.py
+++ api/document/views.py
@@ -9,26 +9,29 @@ router = APIRouter()
 @router.post('/similar_document')
 def query_sim_doc(user_text: UserText):
     try:
-        text = user_text.text
+        texts = user_text.text
+        text_li = texts.split('\n\n')
+        res_per_pgh = []
+        for text in text_li:
+            sim_doc_id = query_pinecone_document(text, top_k=5)
+            if sim_doc_id is None:
+                raise Exception
+            print(sim_doc_id)
+            res_list = []
+            for item in sim_doc_id:
+                id = int(item['id'])
+                score = item['score']
+                table_data = search_document_by_id(id)
+                if table_data is None:
+                    continue
+                reference = make_reference(table_data['title'], table_data['writer'], table_data['date'],
+                                        table_data['from_'], table_data['url'])
+                data = {'title': table_data['title'], 'writer': table_data['writer'], 'date': table_data['date'],
+                        'from': table_data['from_'], 'url': table_data['url'], 'reference': reference}
+                res_list.append(data)
+            res_per_pgh.append(res_list)
 
-        sim_doc_id = query_pinecone_document(text, top_k=5)
-        if sim_doc_id is None:
-            raise Exception
-        print(sim_doc_id)
-        res_list = []
-        for item in sim_doc_id:
-            id = int(item['id'])
-            score = item['score']
-            table_data = search_document_by_id(id)
-            if table_data is None:
-                continue
-            reference = make_reference(table_data['title'], table_data['writer'], table_data['date'],
-                                       table_data['from_'], table_data['url'])
-            data = {'title': table_data['title'], 'writer': table_data['writer'], 'date': table_data['date'],
-                    'from': table_data['from_'], 'url': table_data['url'], 'reference': reference}
-            res_list.append(data)
-
-        return {'success': True, 'documents': res_list}
+        return {'success': True, 'documents': res_per_pgh}
     except Exception as e:
         print(e)
         return {'success': False}
@@ -37,22 +40,25 @@ def query_sim_doc(user_text: UserText):
 @router.post('/similar_law')
 def query_sim_law(user_text: UserText):
     try:
-        text = user_text.text
+        texts = user_text.text
+        text_li = texts.split('\n\n')
+        res_per_pgh = []
+        for text in text_li:
+            sim_law_id = query_pinecone_law(text, top_k=5)
+            if sim_law_id is None:
+                raise Exception
+            print(sim_law_id)
+            res_list = []
+            for item in sim_law_id:
+                id = int(item['id'])
+                table_data = search_law_by_id(id)
+                if table_data is None:
+                    continue
+                data = {'title': table_data['title'].strip(), 'url': table_data['url']}
+                res_list.append(data)
+            res_per_pgh.append(res_list)
 
-        sim_law_id = query_pinecone_law(text, top_k=5)
-        if sim_law_id is None:
-            raise Exception
-        print(sim_law_id)
-        res_list = []
-        for item in sim_law_id:
-            id = int(item['id'])
-            table_data = search_law_by_id(id)
-            if table_data is None:
-                continue
-            data = {'title': table_data['title'].strip(), 'url': table_data['url']}
-            res_list.append(data)
-
-        return {'success': True, 'laws': res_list}
+        return {'success': True, 'laws': res_per_pgh}
     except Exception as e:
         print(e)
         return {'success': False}
